657dfa7eec9a5fcf53c07b6d08ceba2f224120e2
akshayblevel <akshayblevel@gmail.com>
2021-07-30 20:49:32 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ts-pjm9js
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-pjm9js)
---
+++ src/app.tsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+
+function Counter() {
+  const [count,setCount] = useState(0);
+
+  function increase() {
+    setCount(count+1);
+  }
+
+  return(
+    <div>
+      My Count is {count} <br/>
+      <button onClick={increase}>+</button>
+    </div>
+  )
+}
+
+function App() {
+return (
+  <div>
+    <Counter/>
+  </div>
+);
+}
+
+export default App;
